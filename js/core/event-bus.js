c92a1328228b020127ed787474a4381e92833186
mohamedsamysaadoun3 <mohamedsamysaadoun3@gmail.com>
2025-05-17 13:03:52 +0300

    Update event-bus.js

--- js/core/event-bus.js
+++ js/core/event-bus.js
@@ -1,1 +1,38 @@
+// js/core/event-bus.js
+// (Optional Advanced Feature)
+// A simple event bus for decoupled inter-module communication.
 
+/*
+const events = {};
+
+export function subscribe(eventName, callback) {
+    if (!events[eventName]) {
+        events[eventName] = [];
+    }
+    events[eventName].push(callback);
+    // console.log(`[Event Bus] Subscribed to "${eventName}"`);
+}
+
+export function publish(eventName, data) {
+    if (events[eventName]) {
+        // console.log(`[Event Bus] Publishing "${eventName}" with data:`, data);
+        events[eventName].forEach(callback => {
+            try {
+                callback(data);
+            } catch (error) {
+                console.error(`[Event Bus] Error in subscriber for "${eventName}":`, error);
+            }
+        });
+    }
+}
+
+export function unsubscribe(eventName, callbackToRemove) {
+    if (events[eventName]) {
+        events[eventName] = events[eventName].filter(callback => callback !== callbackToRemove);
+        // console.log(`[Event Bus] Unsubscribed from "${eventName}"`);
+    }
+}
+*/
+
+console.log("[Event Bus] Module loaded (currently not actively used).");
+export default {};
